9d9658f1bd89435975d8c472efc761cf70dc250f
Rafael <rafaeldepaulamaia@yahoo.com.br>
2021-09-02 13:51:33 -0300

    Implementando a base do gráfico na primeira tela da home

--- app/src/main/java/com/android/cryptomanager/home/view/fragments/FragmentoUm.kt
+++ app/src/main/java/com/android/cryptomanager/home/view/fragments/FragmentoUm.kt
@@ -1,11 +1,17 @@
 package com.android.cryptomanager.home.view.fragments
 
+import android.graphics.Color
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.android.cryptomanager.databinding.FragmentoumFragmentBinding
+import com.github.mikephil.charting.animation.Easing
+import com.github.mikephil.charting.charts.PieChart
+import com.github.mikephil.charting.data.PieData
+import com.github.mikephil.charting.data.PieDataSet
+import com.github.mikephil.charting.data.PieEntry
 
 
 class FragmentoUm : Fragment() {
@@ -13,6 +19,7 @@ class FragmentoUm : Fragment() {
     private var _binding: FragmentoumFragmentBinding? = null
     private val binding get() = _binding!!
 
+    var pieChart: PieChart? = null
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -20,7 +27,76 @@ class FragmentoUm : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentoumFragmentBinding.inflate(layoutInflater)
+        pieChart = binding.pieChartView
         return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initPieChart()
+        showPieChart()
+    }
+
+    private fun initPieChart() {
+        //using percentage as values instead of amount
+        pieChart!!.setUsePercentValues(true)
+
+        //remove the description label on the lower left corner, default true if not set
+        pieChart!!.description.isEnabled = false
+
+        //enabling the user to rotate the chart, default true
+        pieChart!!.isRotationEnabled = true
+        //adding friction when rotating the pie chart
+        pieChart!!.dragDecelerationFrictionCoef = 0.9f
+        //setting the first entry start from right hand side, default starting from top
+        pieChart!!.rotationAngle = 0f
+
+        //highlight the entry when it is tapped, default true if not set
+        pieChart!!.isHighlightPerTapEnabled = true
+        //adding animation so the entries pop up from 0 degree
+        pieChart!!.animateY(1400, Easing.EaseInOutQuad)
+        //setting the color of the hole in the middle, default white
+        pieChart!!.setHoleColor(Color.parseColor("#000000"))
+    }
+
+    private fun showPieChart() {
+        val pieEntries: ArrayList<PieEntry> = ArrayList()
+        val label = "type"
+
+        //initializing data
+        val typeAmountMap: MutableMap<String, Int> = HashMap()
+        typeAmountMap["Toys"] = 200
+        typeAmountMap["Snacks"] = 230
+        typeAmountMap["Clothes"] = 100
+        typeAmountMap["Stationary"] = 500
+        typeAmountMap["Phone"] = 50
+
+        //initializing colors for the entries
+        val colors: ArrayList<Int> = ArrayList()
+        colors.add(Color.parseColor("#304567"))
+        colors.add(Color.parseColor("#309967"))
+        colors.add(Color.parseColor("#476567"))
+        colors.add(Color.parseColor("#890567"))
+        colors.add(Color.parseColor("#a35567"))
+        colors.add(Color.parseColor("#ff5f67"))
+        colors.add(Color.parseColor("#3ca567"))
+
+        //input data and fit data into pie chart entry
+        for (type in typeAmountMap.keys) {
+            pieEntries.add(PieEntry(typeAmountMap[type]!!.toFloat(), type))
+        }
+
+        //collecting the entries with label name
+        val pieDataSet = PieDataSet(pieEntries, label)
+        //setting text size of the value
+        pieDataSet.valueTextSize = 12f
+        //providing color list for coloring different entries
+        pieDataSet.colors = colors
+        //grouping the data set from entry to chart
+        val pieData = PieData(pieDataSet)
+        //showing the value of the entries, default true if not set
+        pieData.setDrawValues(true)
+        pieChart!!.data = pieData
+        pieChart!!.invalidate()
+    }
 }
